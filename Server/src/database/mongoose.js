fb9d6b4c93f5a71fae69cd7c6e050f54b88f10b5
Shanks lll <shanksx00@gmail.com>
2022-07-29 17:26:30 +0200

    first commit

--- Server/src/database/mongoose.js
+++ Server/src/database/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb+srv://Shanks:a5BN00xo@cluster0.vvrbi.mongodb.net/chat')
+// old = mongodb+srv://Shanks:a5BN00xo@cluster0.vvrbi.mongodb.net/chat
+mongoose.connect('mongodb+srv://Shanks:a5BN00xo@cluster0.vvrbi.mongodb.net/chat?retryWrites=true&w=majority')
     .then(() => console.log("Database connected"))
     .catch((error) => console.log(error))
 
